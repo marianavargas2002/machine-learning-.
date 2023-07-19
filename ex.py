202b9d823f1f0829e7192a040177093fbdd901da
marianavargas2002 <vargasc.mariana@uces.edu.co>
2023-07-19 11:49:04 -0500

    ejercicio1

---
+++ ex.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import cv2 
+# ejercicios de ML
+
+
+def linea(lado):
+    im = np.zeros((lado,lado,3),np.uint8)
+
+    for i in range(lado):
+        im[i,i] = [255,255,255]
+
+    cv2.imwrite('ex1.png',im)
+
+linea(100)
